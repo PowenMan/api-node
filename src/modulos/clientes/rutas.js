9aca3d15f851bfca590d5e95ba2777d2e8859245
Manuel Davila <davilamanuel03914@gmail.com>
2024-05-16 20:06:16 -0500

    Agrego metodos para recibir el id para lsitar usuario archivo rutas.js

--- src/modulos/clientes/rutas.js
+++ src/modulos/clientes/rutas.js
@@ -6,10 +6,20 @@ const controlador = require('./controlador');
 const router = express.Router();
 
 router.get('/', todos);
+router.get('/:id', uno);
 
 async function todos(req, res) {
     try{
         const item = await controlador.todos();
+        respuesta.success(req, res, item, 200);
+    }catch(err){
+        respuesta.error(req, res, err, 500);
+    }
+}
+
+async function uno(req, res) {
+    try{
+        const item = await controlador.uno();
         respuesta.success(req, res, item, 200);
     }catch(err){
         respuesta.error(req, res, err, 500);
